d5ad9fc02e25c535551dc870e35f9629d509fe95
femelo22 <luiz123jfmg@gmail.com>
2022-02-28 20:55:33 -0300

    feat: mapped pessoa class to db

--- src/main/java/com/lfmelo/domain/Pessoa.java
+++ src/main/java/com/lfmelo/domain/Pessoa.java
@@ -1,26 +1,49 @@
 package com.lfmelo.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.lfmelo.enums.EPerfil;
 
-public abstract class Pessoa {
-	
+@Entity(name = "TB_PESSOA")
+public abstract class Pessoa implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected Integer id;
 	
+	@Column(name = "nome")
 	protected String nome;
 	
+	@Column(name = "cpf", unique = true)
 	protected String cpf;
 	
+	@Column(name = "email", unique = true)
 	protected String email;
 	
+	@Column(name = "senha")
 	protected String senha;
 	
+	@ElementCollection(fetch = FetchType.EAGER) //assegura que a lista de perfis virá junto ao usuário
+	@CollectionTable(name = "PERFIS")
 	protected Set<Integer> perfis = new HashSet<>();
 	
+	@Column(name = "data_criacao")
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	protected LocalDate dataCriacao = LocalDate.now();
 
 	public Pessoa() {
