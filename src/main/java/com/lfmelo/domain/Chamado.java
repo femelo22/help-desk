21b0b8a6e3f4b514423e718c7c8bee29deb7c75f
femelo22 <luiz123jfmg@gmail.com>
2022-02-28 21:03:24 -0300

    feat: annotations jpa to domain class

--- src/main/java/com/lfmelo/domain/Chamado.java
+++ src/main/java/com/lfmelo/domain/Chamado.java
@@ -1,28 +1,58 @@
 package com.lfmelo.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.lfmelo.enums.EPrioridade;
 import com.lfmelo.enums.EStatus;
 
-public class Chamado {
+@Entity
+public class Chamado implements Serializable {
+	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	@Column(name = "data_abertura")
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataAbertura = LocalDate.now();
 	
+	@Column(name = "data_fechamento")
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento;
 	
+	@Column(name = "prioridade")
+	@Enumerated(EnumType.STRING)
 	private EPrioridade prioridade;
 	
+	@Column(name = "status")
+	@Enumerated(EnumType.STRING)
 	private EStatus status;
 	
+	@Column(name = "titulo")
 	private String titulo;
 	
+	@Column(name = "observacoes")
 	private String observacoes;
 
+	@ManyToOne
+	@JoinColumn(name = "tecnico_id")
 	private Tecnico tecnico;
 	
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
 	
 
--- src/main/java/com/lfmelo/domain/Tecnico.java
+++ src/main/java/com/lfmelo/domain/Tecnico.java
@@ -3,16 +3,26 @@ package com.lfmelo.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.lfmelo.enums.EPerfil;
+
+@Entity
 public class Tecnico extends Pessoa {
+	private static final long serialVersionUID = 1L;
 	
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super();
+		addPerfil(EPerfil.CLIENTE);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfil(EPerfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
