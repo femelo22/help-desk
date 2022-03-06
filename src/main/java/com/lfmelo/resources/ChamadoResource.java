eff0d168fd3eafdd12aa9faa4280ba93abc99e3b
femelo22 <luiz123jfmg@gmail.com>
2022-03-06 19:34:31 -0300

    feat: create update and delete

--- src/main/java/com/lfmelo/resources/ChamadoResource.java
+++ src/main/java/com/lfmelo/resources/ChamadoResource.java
@@ -1,17 +1,26 @@
 package com.lfmelo.resources;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lfmelo.domain.Chamado;
+import com.lfmelo.dtos.ChamadoDTO;
 import com.lfmelo.services.ChamadoService;
 
 @RestController
@@ -31,6 +40,30 @@ public class ChamadoResource {
 	@ResponseStatus(HttpStatus.OK)
 	public ResponseEntity<Chamado> findById(@PathVariable Integer id) {
 		return ResponseEntity.ok().body(this.service.findById(id));
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<ChamadoDTO> crete(@RequestBody @Valid ChamadoDTO dto) {
+		Chamado chamado = this.service.create(dto);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(chamado.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public ResponseEntity<Chamado> update(@RequestBody @Valid ChamadoDTO dto, @PathVariable Integer id) {
+		this.service.update(dto, id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public ResponseEntity<Chamado> delete(@PathVariable Integer id) {
+		this.service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
--- src/main/java/com/lfmelo/services/ChamadoService.java
+++ src/main/java/com/lfmelo/services/ChamadoService.java
@@ -2,10 +2,18 @@ package com.lfmelo.services;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lfmelo.domain.Chamado;
+import com.lfmelo.domain.Cliente;
+import com.lfmelo.domain.Tecnico;
+import com.lfmelo.dtos.ChamadoDTO;
+import com.lfmelo.enums.EPrioridade;
+import com.lfmelo.enums.EStatus;
 import com.lfmelo.exceptions.NotFoundException;
 import com.lfmelo.repositories.ChamadoRepository;
 
@@ -15,12 +23,54 @@ public class ChamadoService {
 	@Autowired
 	private ChamadoRepository repository;
 	
+	@Autowired
+	private ClienteService clienteService;
+	
+	@Autowired
+	private TecnicoService tecnicoService;
+	
 	public Chamado findById(Integer id) {
 		return this.repository.findById(id).orElseThrow(() -> new NotFoundException("Objeto não encontrado id " + id));
 	}
 	
 	public List<Chamado> findAll() {
 		return this.repository.findAll();
+	}
+
+	public Chamado create(ChamadoDTO dto) {
+		return this.repository.save(newChamadoFromDto(dto));
+	}
+	
+	public Chamado newChamadoFromDto(ChamadoDTO dto) {
+		Chamado chamado = new Chamado();
+		Cliente cliente = this.clienteService.findById(dto.getCliente());
+		Tecnico tecnico = this.tecnicoService.findById(dto.getTecnico());
+		
+		if(dto.getId() != null) {
+			chamado.setId(dto.getId());
+		}
+		
+		chamado.setCliente(cliente);
+		chamado.setTecnico(tecnico);
+		chamado.setPrioridade(EPrioridade.toEnum(dto.getPrioridade()));
+		chamado.setStatus(EStatus.toEnum(dto.getStatus()));
+		chamado.setObservacoes(dto.getObservacoes());
+		chamado.setTitulo(dto.getTitulo());
+		
+		return chamado;
+	}
+
+	public void update(@Valid ChamadoDTO dto, Integer id) {
+		Chamado chamado = this.findById(id);
+		BeanUtils.copyProperties(dto, chamado);
+		chamado.setId(id);
+		this.repository.save(chamado);
+	}
+
+	public void delete(Integer id) {
+		Chamado chamado = this.findById(id);
+		this.repository.delete(chamado);
+		
 	}
 	
 	
